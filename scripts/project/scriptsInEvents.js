c3b6660a0d0a5b453c3779da5179c8b5bd8f7ad0
Just Dzhi <95054989+JustDzhi@users.noreply.github.com>
2023-12-30 20:45:52 +0400

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async ["E-Globals_Event12_Act1"](runtime, localVars)
+	{
+		document.title = runtime.projectName + " - " + localVars.window_title_;
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
